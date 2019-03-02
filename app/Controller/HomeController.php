241edba20866e8c6a9257155de2d1f693fc64755
RenChao <donknap@gmail.com>
2019-03-02 16:42:54 +0800

    提交一些示例

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -8,15 +8,30 @@ namespace W7\App\Controller;
 
 
 use W7\Core\Controller\ControllerAbstract;
+use W7\Http\Message\File\File;
 use W7\Http\Message\Server\Request;
 
 class HomeController extends ControllerAbstract {
 	public function index(Request $request) {
 		print_r($request->post());
+		return [
+			'data' => [
+				'uid' => 1,
+				'username' => 'rangine'
+			]
+		];
 		return 'helloWorld';
 	}
 
 	public function userLogin(Request $request, $uid = 0) {
 		return 'user-login ---- uid : ' . $uid . ' --- post: ' . $request->post('password');
+	}
+
+	public function download() {
+		return $this->response()->withFile(new File('/home/data/1.txt'));
+	}
+
+	public function userheader() {
+		return $this->response()->withHeader('Content-Type', 'text/html;charset=utf-8')->withContent('withheader');
 	}
 }
