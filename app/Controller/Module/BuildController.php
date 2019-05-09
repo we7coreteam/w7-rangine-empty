4e726b004fa58d719537132d1ad3121019ed5d6e
titrxw <1120309488@qq.com>
2019-05-09 09:43:46 +0800

    添加Controller demo

---
+++ app/Controller/Module/BuildController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author donknap
+ * @date 19-3-25 下午5:18
+ */
+
+namespace W7\App\Controller\Module;
+
+use W7\Core\Controller\ControllerAbstract;
+use W7\Http\Message\Server\Request;
+
+class BuildController extends ControllerAbstract {
+	public function index(Request $request) {
+
+	}
+}
---
+++ app/Controller/Module/InfoController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author donknap
+ * @date 19-1-7 上午8:24
+ */
+
+namespace W7\App\Controller\Module;
+
+use W7\Core\Controller\ControllerAbstract;
+use W7\Http\Message\Server\Request;
+
+class InfoController extends ControllerAbstract {
+	public function index(Request $request) {
+
+	}
+}
