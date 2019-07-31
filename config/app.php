f92f3c3d439350d78fbc2e3a7e0f0e1fd0fca517
rxw <1120309488@qq.com>
2019-07-31 18:07:40 +0800

    修改错误级别配置名称

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
 		//SETTING_DEVELOPMENT = RELEASE|CLEAR_LOG
 		'env' => ienv('SETTING_DEVELOPMENT', RELEASE),
 		//最新版可用
-		'error_level' => E_ALL ^ E_NOTICE,
+		'error_reporting' => E_ALL ^ E_NOTICE,
 		'basedir' => [
 			'/home/wwwroot/we7/swoole'
 		]
