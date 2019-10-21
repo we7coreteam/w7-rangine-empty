820f73b47641ad8a4e2e35d9b1f59cd25b294d43
titrxw <1120309488@qq.com>
2019-10-21 11:27:29 +0800

    添加session自定义handler测试用例

--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -2,6 +2,7 @@
 
 namespace W7\Tests;
 
+use Illuminate\Filesystem\Filesystem;
 use W7\Core\Session\Session;
 use W7\Http\Message\Server\Request;
 
@@ -15,14 +16,6 @@ class SessionTest extends TestCase {
 	}
 
 	public function testDestroy() {
-		$config = iconfig()->getUserConfig('app');
-		$config['session'] = [
-			'gc_divisor' => 1,
-			'gc_probability' => 1,
-			'expires' => 1
-		];
-		iconfig()->setUserConfig('app', $config);
-
 		$session = new Session();
 		$session->start(new Request('GET', '/'));
 		$session->set('test', 1);
@@ -54,5 +47,23 @@ class SessionTest extends TestCase {
 		$property->setValue($session, null);
 
 		$this->assertSame('', $sessionReflect->getMethod('get')->invokeArgs($session, ['test']));
+	}
+
+	public function testUserHandler() {
+		$filesystem = new Filesystem();
+		$filesystem->copyDirectory(__DIR__ . '/Handler/Session', APP_PATH . '/Handler/Session');
+
+		$config = iconfig()->getUserConfig('app');
+		$config['session']['handler'] = 'test';
+		iconfig()->setUserConfig('app', $config);
+
+		$session = new Session();
+		$session->start(new Request('GET', '/'));
+
+		$session->set('test', 1);
+
+		$this->assertSame(1, $session->get('test'));
+
+		$filesystem->deleteDirectory(APP_PATH . '/Handler/Session');
 	}
 }
