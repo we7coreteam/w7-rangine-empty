86e900ceaff1d9c7d639dc8d553b754faca93c72
titrxw <1120309488@qq.com>
2019-10-22 10:51:38 +0800

    添加view测试用例

--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -2,16 +2,62 @@
 
 namespace W7\Tests;
 
+use Illuminate\Filesystem\Filesystem;
+use W7\Core\View\Handler\TwigHandler;
 use W7\Core\View\View;
 
 class ViewTest extends TestCase {
 	public function testRender() {
 		copy(__DIR__ . '/Provider/view/index.html', APP_PATH . '/View/test.html');
 
-		$content = iloader()->singleton(View::class)->render('test');
+		$content = (new View())->render('test');
 
 		$this->assertSame('ok', $content);
 
 		unlink(APP_PATH . '/View/test.html');
+	}
+
+	public function testNamespace() {
+		$config = iconfig()->getUserConfig('app');
+		$config['view'] = [
+			'template_path' => [
+				'test' => __DIR__ . '/Provider/view'
+			]
+		];
+		iconfig()->setUserConfig('app', $config);
+
+		$view = new View();
+		$handler = new TwigHandler([
+			'debug' => false,
+			'provider_template_path' => [
+				'test' => [__DIR__ . '/Provider/view']
+			]
+		]);
+		$handlerReflect = new \ReflectionClass($handler);
+		$property = $handlerReflect->getProperty('defaultTemplatePath');
+		$property->setAccessible(true);
+		$property->setValue($handler, null);
+
+		$content = $view->render('@test/index');
+
+		$this->assertSame('ok', $content);
+	}
+
+	public function testHandler() {
+		$filesystem = new Filesystem();
+		$filesystem->copyDirectory(__DIR__ . '/Handler/View', APP_PATH . '/Handler/View');
+
+		$config = iconfig()->getUserConfig('app');
+		$config['view'] = [
+			'handler' => 'test'
+		];
+		iconfig()->setUserConfig('app', $config);
+
+		$view = new View();
+		$content = $view->render('index');
+
+		$this->assertSame('a:2:{i:0;s:8:"__main__";i:1;s:10:"index.html";}', $content);
+
+		$filesystem->deleteDirectory(APP_PATH . '/Handler/View');
 	}
 }
