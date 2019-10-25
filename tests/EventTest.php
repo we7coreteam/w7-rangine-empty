17eb1834eeeb4d20cfdfe53008e1f85545cab76d
titrxw <1120309488@qq.com>
2019-10-25 15:08:55 +0800

    添加触发多个event测试用例

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -14,6 +14,23 @@ class EventTest extends TestCase {
 		$this->assertSame(true, $event->hasListeners('test'));
 	}
 
+	public function testMultiEvent() {
+		$event = new EventDispatcher();
+		$event->listen('test', function () {
+			return 'test';
+		});
+		$event->listen('test', function () {
+			return 'test1';
+		});
+		$event->listen('test', function () {
+			return 'test2';
+		});
+
+		$this->assertSame('test', $event->dispatch('test')[0]);
+		$this->assertSame('test1', $event->dispatch('test')[1]);
+		$this->assertSame('test2', $event->dispatch('test')[2]);
+	}
+
 	public function testRunAll() {
 		$event = new EventDispatcher();
 		$event->listen('test', function () {
