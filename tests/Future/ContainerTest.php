6e539a2adce2798c222af3f234b0bfad768ea313
titrxw <1120309488@qq.com>
2019-10-25 12:15:02 +0800

    添加新版container测试用例

---
+++ tests/Future/ContainerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace W7\Tests\Future;
+
+use W7\Tests\TestCase;
+
+class TestContainer {
+	public function __destruct() {
+		echo 'test container destruct';
+	}
+}
+
+
+class ContainerTest extends TestCase {
+	public function testDelete() {
+		iloader()->set('test', true);
+		$this->assertSame(true, iloader()->has('test'));
+
+		iloader()->delete('test');
+		$this->assertSame(false, iloader()->has('test'));
+	}
+
+	public function testClear() {
+		ob_start();
+		$this->clear();
+		$echo = ob_get_clean();
+		$this->assertSame('test container destruct', $echo);
+	}
+
+	public function clear() {
+		iloader()->set('test', true);
+		$this->assertSame(true, iloader()->has('test'));
+
+		iloader()->clear();
+		$this->assertSame(false, iloader()->has('test'));
+
+		iloader()->set('test', TestContainer::class);
+		$class = iloader()->get('test');
+		$this->assertSame(true, $class instanceof TestContainer);
+
+		iloader()->clear();
+	}
+}
