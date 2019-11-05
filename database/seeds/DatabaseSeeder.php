b8f2135628188aa0aff27ea6454e9a1f137805b5
titrxw <1120309488@qq.com>
2019-11-05 17:44:22 +0800

    添加默认的seed

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+use W7\Core\Database\Seed\Seeder;
+
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		//
+	}
+}
