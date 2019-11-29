2f6b3f08703c2a653fee5ee7af2027997068fe55
titrxw <1120309488@qq.com>
2019-11-29 14:32:45 +0800

    添加middleware删除的测试用例

---
+++ tests/Future/MiddlewareTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace W7\Tests\Future;
+
+use W7\Http\Server\Dispatcher;
+use W7\Http\Session\Middleware\SessionMiddleware;
+use W7\Tests\TestCase;
+
+class MiddlewareTest extends TestCase {
+	public function testDelete() {
+		/**
+		 * @var Dispatcher $dispatcher
+		 */
+		$dispatcher = iloader()->get(Dispatcher::class);
+		$mapping = $dispatcher->getMiddlewareMapping();
+
+		$this->assertSame(SessionMiddleware::class, $mapping->beforeMiddleware[0][0]);
+
+		$mapping->deleteMiddleware(SessionMiddleware::class);
+
+		$this->assertSame(0, count($mapping->beforeMiddleware));
+
+		$mapping->addBeforeMiddleware(SessionMiddleware::class);
+
+		$this->assertSame(SessionMiddleware::class, $mapping->beforeMiddleware[1][0]);
+		$mapping->deleteAfterMiddleware(SessionMiddleware::class);
+		$this->assertSame(SessionMiddleware::class, $mapping->beforeMiddleware[1][0]);
+
+		$this->assertSame(SessionMiddleware::class, $mapping->beforeMiddleware[1][0]);
+		$mapping->deleteBeforeMiddleware(SessionMiddleware::class);
+		$this->assertSame(0, count($mapping->beforeMiddleware));
+
+	}
+}
