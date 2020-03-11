d3ef34d5f356a632293f0157591e314bcc6a7959
titrxw <1120309488@qq.com>
2020-03-11 11:27:06 +0800

    修改fpm测试

--- public/server.php
+++ public/server.php
@@ -1,18 +1,6 @@
 <?php
 
 require_once dirname(__DIR__, 1) . '/vendor/autoload.php';
-$script = __FILE__;
-
-$argv = [];
-
-$_SERVER['argv'] = array_merge(
-	[
-		$script,
-		'server:start',
-		'--config-app-setting-server=fpm'
-	],
-	$argv
-);
-
-$app = new \W7\App();
-$app->runConsole();
+new \W7\App();
+$server = new \W7\Fpm\Server\Server();
+$server->start();
