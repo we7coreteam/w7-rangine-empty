9765bc2bd77d286c4decd6ff82959dcbf69fe18b
renchao <donknap@gmail.com>
2020-04-08 19:05:31 +0800

    增加使用示例

--- app/Controller/Home/WelcomeController.php
+++ app/Controller/Home/WelcomeController.php
@@ -13,6 +13,7 @@ namespace W7\App\Controller\Home;
 
 
 use W7\Core\Controller\ControllerAbstract;
+use W7\Http\Message\File\File;
 use W7\Http\Message\Server\Request;
 
 class WelcomeController extends ControllerAbstract {
@@ -31,5 +32,23 @@ class WelcomeController extends ControllerAbstract {
 			return ['id' => $id];
 		}
 		return $request->getQueryParams() ?: 'success';
+	}
+
+	public function testDownloadFile(Request $request) {
+		return $this->response()->withHeaders([
+			'Content-Type' => 'application/x-zip',
+			'Content-Disposition' => "attachment; filename*=UTF-8''123.zip",
+			'Pragma' => 'public',
+			'Cache-Control' => 'public, must-revalidate',
+			'Content-Transfer-Encoding' => 'binary'
+		])->withFile(new File(RUNTIME_PATH . '/123.zip'))->send();
+	}
+
+	public function testWrite(Request $request) {
+		for($i=0;$i<1000;$i++){
+			$this->response()->withContent($i.'</br>')->write();
+			sleep(1);
+		}
+		return 'success';
 	}
 }
