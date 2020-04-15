8892522a7c51b1cc5463f2f41dddfae87c815818
titrxw <1120309488@qq.com>
2020-04-15 16:05:43 +0800

    整理代码格式

--- app/Handler/Session/DbHandler.php
+++ app/Handler/Session/DbHandler.php
@@ -7,34 +7,34 @@ use W7\App\Model\Entity\User\Online;
 use W7\Core\Session\Handler\HandlerAbstract;
 
 class DbHandler extends HandlerAbstract {
-public function write($session_id, $session_data) {
-	$session = Session::query()->where('session_id', '=', $session_id)->first();
-	if (empty($session)) {
-		Session::query()->create([
-			'session_id' => $session_id,
-			'data' => $session_data,
-			'expired_at' => time() + $this->getExpires(),
-		]);
-	} else {
-		$session->data = $session_data;
-		$session->expired_at = time() + $this->getExpires();
-		$session->save();
-	}
-
-	$userInfo = $this->unpack($session_data);
-	if (!empty($userInfo['user'])) {
-		//存储session的时候，再存一份在线用户列表，也可以写入 redis
-		if (!(Online::query()->where('user_id', '=', $userInfo['user']['uid'])->exists())) {
-			Online::query()->create([
-				'user_id' => $userInfo['user']['uid'],
-				'user_name' => $userInfo['user']['username'],
-				'fd' => $userInfo['fd'],
+	public function write($session_id, $session_data) {
+		$session = Session::query()->where('session_id', '=', $session_id)->first();
+		if (empty($session)) {
+			Session::query()->create([
+				'session_id' => $session_id,
+				'data' => $session_data,
+				'expired_at' => time() + $this->getExpires(),
 			]);
+		} else {
+			$session->data = $session_data;
+			$session->expired_at = time() + $this->getExpires();
+			$session->save();
 		}
-	}
 
-	return true;
-}
+		$userInfo = $this->unpack($session_data);
+		if (!empty($userInfo['user'])) {
+			//存储session的时候，再存一份在线用户列表，也可以写入 redis
+			if (!(Online::query()->where('user_id', '=', $userInfo['user']['uid'])->exists())) {
+				Online::query()->create([
+					'user_id' => $userInfo['user']['uid'],
+					'user_name' => $userInfo['user']['username'],
+					'fd' => $userInfo['fd'],
+				]);
+			}
+		}
+
+		return true;
+	}
 
 	public function read($session_id) {
 		if (empty($session_id)) {
