22d8017895a774e42873972d50380759d63457ca
titrxw <1120309488@qq.com>
2020-05-21 11:25:34 +0800

    修复路由兼容问题

--- route/home.php
+++ route/home.php
@@ -10,7 +10,7 @@
  */
 irouter()->middleware('TestMiddleware')->get('/', 'Home\WelcomeController@index');
 
-irouter()->middleware(\W7\App\Middleware\TestMiddleware::class)->group('', function (\W7\Core\Route\Route $route) {
+irouter()->middleware(\W7\App\Middleware\TestMiddleware::class)->group('', function (\W7\Core\Route\Router $route) {
 	$route->any('/home/api-get[/{id:\d+}]', [\W7\App\Controller\Home\WelcomeController::class, 'apiGet']);
 });
 
