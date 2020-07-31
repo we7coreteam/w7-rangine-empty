fe1106ce83c39f1897956cd99e781d4c799c76cf
titrxw <1120309488@qq.com>
2020-07-31 18:43:58 +0800

    添加门面实例清空注释

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -19,6 +19,7 @@ class TestCase extends \PHPUnit\Framework\TestCase {
 	public function initApp() {
 		require_once __DIR__ . '/../config/define.php';
 		new App();
+		//这里加清空的原因是，如果多个测试用例同时运行，如果使用门面，会有实例无法释放问题
 		FacadeAbstract::$resolvedInstance = [];
 		App::$self = null;
 	}
