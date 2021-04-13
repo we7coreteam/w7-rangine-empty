5a64335d0af307ec5a503e4ae36bcf3ac4af83f3
titrxw <1120309488@qq.com>
2021-04-13 10:24:32 +0800

    修改fpm入口文件

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@
  */
 
 require_once dirname(__DIR__, 1) . '/vendor/autoload.php';
+require_once dirname(__DIR__, 1) . '/config/define.php';
 
 $server = new \W7\Fpm\Server\Server();
 new \W7\App();
