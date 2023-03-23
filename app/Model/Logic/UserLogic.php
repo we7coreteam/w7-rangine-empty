b8b321c0a17ae62e4d1edd2e7a0bf36b4eadf21c
rxwyun <1120309488@qq.com>
2023-03-23 16:56:57 +0800

    修改废弃instance用法

--- app/Model/Logic/UserLogic.php
+++ app/Model/Logic/UserLogic.php
@@ -1,14 +1,22 @@
 <?php
 
+/**
+ * WeEngine Api System
+ *
+ * (c) We7Team 2019 <https://www.w7.cc>
+ *
+ * This is not a free software
+ * Using it under the license terms
+ * visited https://www.w7.cc for more details
+ */
 
 namespace W7\App\Model\Logic;
 
-
 use W7\Core\Database\LogicAbstract;
-use W7\Core\Helper\Traiter\InstanceTraiter;
+use W7\Core\Helper\Traiter\InstanceTrait;
 
 class UserLogic extends LogicAbstract {
-	use InstanceTraiter;
+	use InstanceTrait;
 
 	private $user = [
 		[
